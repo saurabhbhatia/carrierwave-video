d4ae394cabf46443178e414d6db499ff02adbafa
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2014-01-01 13:50:59 +0800

    Removed 'qscale' from default options in mp4

--- lib/carrierwave/video/ffmpeg_options.rb
+++ lib/carrierwave/video/ffmpeg_options.rb
@@ -80,7 +80,7 @@ module CarrierWave
             when 'mp4'
               h[:video_codec] = 'libx264'
               h[:audio_codec] = 'libfaac'
-              h[:custom] = '-qscale 0 -preset slow -g 30'
+              h[:custom] = '-preset slow -g 30'
             when 'ogv'
               h[:video_codec] = 'libtheora'
               h[:audio_codec] = 'libvorbis'
